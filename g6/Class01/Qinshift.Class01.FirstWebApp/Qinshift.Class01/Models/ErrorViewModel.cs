92b173185089b9af780f0e15732520f35d8dfe16
Martin <panovski.martin93@gmail.com>
2024-06-04 21:00:31 +0200

    First class demo code added

---
+++ g6/Class01/Qinshift.Class01.FirstWebApp/Qinshift.Class01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Qinshift.Class01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
