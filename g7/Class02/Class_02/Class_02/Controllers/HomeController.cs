589bf80e6c8186b70b9b5435d18da75313e894ab
tpelivanov <t.pelivanov@linkplus-it.com>
2024-06-06 20:56:39 +0200

    class02_Controllers_ActionResult_Routing

---
+++ g7/Class02/Class_02/Class_02/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Class_02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Class_02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpPost] => will wait for post request
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpGet] => this means that the Privacy method bellow will wait for a get request
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
