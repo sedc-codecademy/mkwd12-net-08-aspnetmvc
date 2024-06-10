619700a4c50c525aee7352a765ad4f13e3de1fbf
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2024-06-10 20:55:43 +0200

    Class 04

---
+++ g3/Class04/Class04_DemoApp/Class04_DemoApp/StaticDb.cs
@@ -0,0 +1,27 @@
+﻿using ViewModels;
+
+namespace Class04_DemoApp
+{
+    public static class StaticDb
+    {
+        public static List<UserViewModel> Users = new List<UserViewModel>
+        {
+            new UserViewModel
+            {
+                Id = 1,
+                FirstName = "Risto",
+                LastName = "Panchevski"
+            },new UserViewModel
+            {
+                Id = 2,
+                FirstName = "Slave",
+                LastName = "Ivanovski"
+            },new UserViewModel
+            {
+                Id = 3,
+                FirstName = "Petko",
+                LastName = "Petkovski"
+            }
+        };
+    }
+}
