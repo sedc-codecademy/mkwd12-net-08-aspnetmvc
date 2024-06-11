9833d69dc11b18e0a7c84e8674a917e9be4615a2
tijanastojanovska <stojanovska_tijana@outlook.com>
2024-06-11 20:46:38 +0200

    Class 04

---
+++ g6/Class 04/PizzaApp/PizzaApp/Models/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PizzaApp.Models.Enums;
+
+namespace PizzaApp.Models.ViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public string PizzaName { get; set; }
+        public string UserFullName { get; set; }
+        public int Price { get; set; }
+        public PaymentMethodEnum PaymentMethod { get; set; }
+    }
+}
