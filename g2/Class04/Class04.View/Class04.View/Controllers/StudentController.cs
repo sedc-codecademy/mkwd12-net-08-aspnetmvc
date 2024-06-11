9af670e7d58a78b89841c7f53c78898b190567d7
Danilo Borozan <daniloborozan07@gmail.com>
2024-06-11 21:01:05 +0200

    add code

--- g2/Class04/Class04.View/Class04.View/Controllers/StudentController.cs
+++ g2/Class04/Class04.View/Class04.View/Controllers/StudentController.cs
@@ -1,5 +1,7 @@
 ﻿using Class04.View.Database;
 using Class04.View.Models.DTOModels;
+using Class04.View.Models.Entities;
+using Class04.View.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Class04.View.Controllers
@@ -12,6 +14,41 @@ namespace Class04.View.Controllers
         {
             var studetns = InMemoryDatabase.Students.Select(x => new StudentWithCourseDTO(x.Id, x.FirstName, x.LastName, x.DateOfBirth, x.ActiveCourse.Id, x.ActiveCourse.Name));
             return View(studetns);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(int id) 
+        {
+            var student = InMemoryDatabase.Students.FirstOrDefault(x => x.Id == id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+            var studentWithCourse = new StudentWithCourseDTO(student.Id, student.FirstName, student.LastName, student.DateOfBirth, student.ActiveCourse.Id,
+                student.ActiveCourse.Name);
+
+            return View(studentWithCourse);
+        }
+
+        [HttpGet("create")]
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+        [HttpPost("create")]
+        public IActionResult CreateStudent(CreateStudentVM createStudentVM)
+        {
+            var studentEntity = new Student()
+            {
+                Id = InMemoryDatabase.Students.Count + 1,
+                FirstName = createStudentVM.FirstName,
+                LastName = createStudentVM.LastName,
+                DateOfBirth = createStudentVM.DateOfBirth,
+                ActiveCourse = InMemoryDatabase.Courses[3]
+            };
+
+            InMemoryDatabase.Students.Add(studentEntity);
+            return RedirectToAction("Index");
         }
     }
 }
