64f8842eaf7619a91be28f603a6f053a18b9ff68
Danilo Borozan <daniloborozan07@gmail.com>
2024-06-18 18:23:43 +0200

    add

---
+++ g2/Class06/TodoApplication/TodoApplication.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoApplication.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
