e83fce6f48c9b316e4ce7a287050376e7e8283ba
Danilo Borozan <daniloborozan07@gmail.com>
2024-06-18 19:01:02 +0200

    add

--- g2/Class06/TodoApplication/TodoApplication.Dtos/FilterDto.cs
+++ g2/Class06/TodoApplication/TodoApplication.Dtos/FilterDto.cs
@@ -10,7 +10,7 @@ namespace TodoApplication.Dtos
     {
         public List<CategoryDto> Categories { get; set; }
         public List<StatusDto> Statuses { get; set; }
-        public int CategoryId { get; set; }
-        public int StatusId { get; set; }
+        public int CategoryId { get; set; } = 0;
+        public int StatusId { get; set; } = 0;
     }
 }
