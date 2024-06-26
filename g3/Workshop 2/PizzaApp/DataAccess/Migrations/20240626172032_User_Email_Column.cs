b424cb468ee0459f0cba5343871aa9a0e0000dd8
risto-panchevski <76558034+risto-panchevski@users.noreply.github.com>
2024-06-26 19:26:37 +0200

    Class 09

---
+++ g3/Workshop 2/PizzaApp/DataAccess/Migrations/20240626172032_User_Email_Column.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class User_Email_Column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+        }
+    }
+}
